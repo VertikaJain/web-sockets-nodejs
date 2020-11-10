db1d7aefdd371bfca34321d0310d89bdf7e673c1
Vertika Jain <vertikajain96@gmail.com>
2020-11-10 23:51:04 +0530

    Socket IO setup #Day94

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let express = require("express")
+let socket = require("socket.io")
+
 let app = express()
 const PORT = 4000
-let server = app.listen(PORT, () => console.log("Listening to SErver on port 4000..."))
+let server = app.listen(PORT, () => console.log("Listening to Server on port 4000..."))
 
 app.use(express.static("public"))
+
+// Socket setup
+let io = socket(server)
+io.on("connection", socket => {
+    console.log("made socket connection with socket ID:", socket.id);
+})
---
+++ public/chat.js
@@ -0,0 +1,3 @@
+// CLIENT
+// Make connection to the server
+let server = io.connect("http://localhost:4000") // getting io from the library
